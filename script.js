10caa17d8855173e18c0b7a993709764537aec7a
julianapedroso <juliana.cristina@hotmail.com.br>
2021-05-13 23:03:00 -0300

    ajustes no card-items, inclusão do botão remove e lógica do deleteItem criada

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-const taskList = document.getElementById('ul-item')
+const taskList = document.getElementById("ul-item");
 
-function addTask(){
-  const task = document.getElementById('task')
-  const taskValue = task.value
-  if(taskValue) {
-    taskList.innerHTML += `<div class=\"list-item-box\"><li class=\"list-item\">${taskValue}</li><a href=\"#\" class=\"delete-item\"><i class=\"fa fa-remove\"></i></a></div>`
+function addTask() {
+  const task = document.getElementById("task");
+  const taskValue = task.value;
+  if (taskValue) {
+    taskList.innerHTML += `<div class=\"list-item-box\"><li class=\"list-item\">${taskValue}</li><a href=\"#\" class=\"delete-item\"><img src="/assets/minus.svg" class=\"fa fa-remove\"></a></div>`;
   } else {
-    alert('Insira uma tarefa!') 
+    alert("Insira uma tarefa!");
   }
-  task.value = ''
+  task.value = "";
 }
 
 function clearTasks() {
-  if(confirm('Tem certeza que deseja apagar todos os itens?')) {
-    taskList.innerHTML = ''
+  if (confirm("Tem certeza que deseja apagar todos os itens?")) {
+    taskList.innerHTML = "";
   }
 }
 
-taskList.addEventListener('click', deleteItem)
+taskList.addEventListener("click", deleteItem);
 
-function deleteItem(e) {
-  
-  if(e.target.parentElement.classList.contains('delete-item')) {
-    if(confirm('Tem certeza que deseja remover a tarefa?')) {
-      e.target.parentElement.parentElement.remove()
+function deleteItem(event) {
+  if (event.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Tem certeza que deseja remover a tarefa?")) {
+      event.target.parentElement.parentElement.remove();
     }
   }
 }
-
