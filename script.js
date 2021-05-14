673111d5c811aca2dc4db281a43d8cd90b57ed2a
julianapedroso <juliana.cristina@hotmail.com.br>
2021-05-13 21:25:30 -0300

    criação da estrutura HTML e começo da estilização com CSS da tasks list e começo da lógica com JavaScript

---
+++ script.js
@@ -0,0 +1,30 @@
+const taskList = document.getElementById('ul-item')
+
+function addTask(){
+  const task = document.getElementById('task')
+  const taskValue = task.value
+  if(taskValue) {
+    taskList.innerHTML += `<div class=\"list-item-box\"><li class=\"list-item\">${taskValue}</li><a href=\"#\" class=\"delete-item\"><i class=\"fa fa-remove\"></i></a></div>`
+  } else {
+    alert('Insira uma tarefa!') 
+  }
+  task.value = ''
+}
+
+function clearTasks() {
+  if(confirm('Tem certeza que quer apagar tudo?')) {
+    taskList.innerHTML = ''
+  }
+}
+
+taskList.addEventListener('click', deleteItem)
+
+function deleteItem(e) {
+  
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Tem certeza que deseja remover a tarefa?')) {
+      e.target.parentElement.parentElement.remove()
+    }
+  }
+}
+
